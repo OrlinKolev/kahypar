2f1d120a83b2afa610d8c4526ef1855b057a3b83
Orlin Kolev <orlinkolev@gmail.com>
2018-01-19 03:51:40 +0200

    interval soft gain performance fix (runtime -12%)

--- kahypar/partition/refinement/2way_th_soft_gain_refiner.h
+++ kahypar/partition/refinement/2way_th_soft_gain_refiner.h
@@ -145,7 +145,9 @@ class TwoWayThSoftGainRefiner final : public IRefiner,
           _gain_cache.setNotCached(pin);
         }
       }
+    }
 
+    for (const HypernodeID& hn : refinement_nodes) {
       for (const HyperedgeID& he : _hg.incidentEdges(hn)) {
         for (const HypernodeID& pin : _hg.pins(he)) {
           if (!_gain_cache.isCached(pin)) {
